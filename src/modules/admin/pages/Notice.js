8859b81ebeac5ca82ad374f3f02f51f80f095ac4
webdev0101 <web.developers.2021.2024@gmail.com>
2024-01-02 11:54:40 +0530

    gfhgf

--- src/modules/admin/pages/Notice.js
+++ src/modules/admin/pages/Notice.js
@@ -1,11 +1,111 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Notice() {
+const Notice = () => {
+  const [isModal, setIsModal] = useState(false);
+
+  const handleModal = () => {
+    setIsModal(!isModal);
+  };
   return (
-    <div>
-      admin Notice page
-    </div>
-  )
-}
+    <>
+      {isModal && (
+        <div className=" justify-center items-center flex ">
+          <div className="">
+            <div className="  ">
+              <div className="h-96 w-96 bg-white fixed top-0 left-0 px-10 md:mt-20 md:left md:p-0 lg:left-[40%] mt-[20%]  ">
+                <form className="bg-blue-200 shadow-md rounded px-8 pt-6 pb-8 mb-4 ">
+                  <div className="mb-4">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                     
+                    >
+                      Tittle
+                    </label>
+                    <input
+                      name="title"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="title"
+                      type="text"
+                      placeholder="title"
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="email"
+                    >
+                      Link
+                    </label>
+                    <input
+                      name="link"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                      id="link"
+                      type="link"
+                      placeholder="link"
+                    />
+                  </div>
+                  <div className="mb-6">
+                    <label
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="description"
+                    >
+                      Description
+                    </label>
+                    <textarea
+                      name="description"
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                      id="description"
+                      type="text"
+                      placeholder="Type here..."
+                    />
+                  </div>
 
-export default Notice
+                  <div className="flex items-center justify-between">
+                    <button
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      type="submit"
+                    >
+                      Save
+                    </button>
+                    <button
+                    onClick={handleModal}
+                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                      type="button"
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="h-screen w-screen">
+        <div className="m-7 flex justify-between">
+          <h1 className=" text-3xl font-bold  text-center ">Notice</h1>
+          <button
+            onClick={handleModal}
+            className="bg-blue-400 rounded-xl px-5  flex justify-center items-center gap-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-plus-circle"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+            </svg>
+
+            <span>New</span>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Notice;
