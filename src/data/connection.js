e4f76ec2fffef9ff7850656fbbc46982ae1026f0
sanju-react <154334697+sanju-react@users.noreply.github.com>
2024-03-04 14:39:52 +0530

    Changed database connection from "freedb_G-Campus" to "gcampus".

--- src/data/connection.js
+++ src/data/connection.js
@@ -1,10 +1,16 @@
 var mysql = require("mysql2");
 
+// var pool = mysql.createPool({
+//   host: "sql.freedb.tech",
+//   user: "freedb_ask_developers",
+//   password: "nEy%P*St5Y$cbFH",
+//   database: "freedb_G-Campus",
+// });
 var pool = mysql.createPool({
-  host: "sql.freedb.tech",
-  user: "freedb_ask_developers",
-  password: "nEy%P*St5Y$cbFH",
-  database: "freedb_G-Campus",
+  host: "localhost",
+  user: "root",
+  password: "2002",
+  database: "gcampus",
 });
 
 module.exports = pool;
