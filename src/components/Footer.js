dee6823bf6eed2cdc8491b2a133b1d4667cc4093
sanju-react <154334697+sanju-react@users.noreply.github.com>
2024-03-05 09:37:44 +0530

    Update social media icons and links in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -186,7 +186,7 @@ function Footer({ NavLink, MoreOptionToggle, setMoreOptionToggle }) {
               @ASK_DEVELOPERS
             </a>
           </p>
-          <span className="w-1/4 inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-end">
+          <span className="w-1/4 inline-flex gap-3 sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-end">
             <a
               className="text-gray-400"
               href="mailto:web.developers.2021.2024@gmail.com"
@@ -212,14 +212,14 @@ function Footer({ NavLink, MoreOptionToggle, setMoreOptionToggle }) {
               rel="noopener noreferrer"
             >
               <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
                 fill="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-5 h-5"
-                viewBox="0 0 24 24"
+                class="bi bi-twitter-x"
+                viewBox="0 0 16 16"
               >
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
               </svg>
             </a>
           </span>
