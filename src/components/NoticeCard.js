aaf9eadb22397e4fd244ff2777eb3feb444e0ee7
sanju-react <154334697+sanju-react@users.noreply.github.com>
2024-03-11 21:55:32 +0530

    Update the NoticeCard component to display a grid of notices with date, title, and an image.

--- src/components/NoticeCard.js
+++ src/components/NoticeCard.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
+import newgif from "../resources/gifs/new-gif.gif";
 
 const NoticeCard = () => {
-  return (
-    <div>NoticeCard</div>
-  )
-}
+  const notice = [
+    {
+      title: "S.Y.B.C.A SEM 4 Theory Examination Marks (Out of 50)",
 
-export default NoticeCard
+      date: "2021-11-11",
+    },
+
+    {
+      title: "S.Y.B.C.A SEM 4 Theory Examination Marks (Out of 50)",
+
+      date: "2021-11-11",
+    },
+    {
+      title: "S.Y.B.C.A SEM 4 Theory Examination Marks (Out of 50)",
+ 
+      date: "2021-11-11",
+    },
+  ];
+
+  return (
+    <>
+      <div className="h-screen w-screen grid-cols-2 px-10 py-20 flex gap-6">
+        {notice.map((val, index) => (
+          <div
+            key={index}
+            className="h-[50%] w-[40%] rounded-lg border shadow-md py-5 px-5 cursor-pointer hover:shadow-xl"
+          >
+            {/* for Date */}
+            <div className="h-[6%] w-full flex justify-center py-10 items-center">
+              <input type="date" className="rounded-md p-1" value={val.date} />
+            </div>
+            {/* Image container */}
+            <div className="flex justify-center">
+              <img src={newgif} className="h-[4vh] w-auto" alt="Notice" />
+            </div>
+            {/* Title */}
+            <div className="h-[10vh] py-5 px-5  w-full text-3xl">
+              {val.title}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default NoticeCard;
