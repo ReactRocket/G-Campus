c9921fe8c2a90df6a4c23942308df37006637c75
AYUSH VARMA <153015142+ReactRocket@users.noreply.github.com>
2024-03-13 10:50:15 +0530

    Refactor admin panel UI.

--- src/modules/admin/pages/Faculty.js
+++ src/modules/admin/pages/Faculty.js
@@ -30,18 +30,18 @@ const Faculty = () => {
         <h1 className="lg:w-[93%] md:w-[85%] w-3/4 text-2xl font-semibold font-sans">
           Faculty
         </h1>
-        <button className="lg:w-[7%] md:w-[15%] w-1/4 bg-blue-300 rounded-md font-medium flex justify-evenly items-center">
+        <button className="lg:w-[10%] md:w-[15%] w-1/4 rounded-lg shadow hover:bg-blue-500 cursor-pointer scale-90 hover:scale-95 transition-all duration-1000 bg-blue-600 text-white py-2 px-4 font-medium  gap-2 flex justify-center items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="20"
+            height="20"
             fill="currentColor"
             className="bi bi-plus-circle"
             viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
           </svg>
-          <span>New</span>
+          <span className="text-lg">New</span>
         </button>
       </nav>
       <section className="h-full mt-5 grid gap-5 lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
